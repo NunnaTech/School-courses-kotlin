6d42f6f62864bcf42c60f51272a68b45e806218e
unknown <avila.gandy@gmail.com>
2022-05-18 17:12:00 -0500

    Modulo terminado

--- src/CrudCooking/Crud.kt
+++ src/CrudCooking/Crud.kt
@@ -3,25 +3,30 @@ package CrudCooking
 import listaCookingCourse
 
 class Crud {
-    fun addNewCourse(name:String, description:String){
-        if (name.isNotEmpty()&& description.isNotEmpty()){
-            val newEntry = CookingCourse.newEntry(name, description)
-            listaCookingCourse.add(newEntry)
-            CookingCourse.message("El curso con el nombre ${newEntry.name} se registro correctamente")
+    fun addNewCourse(name:String, description:String):Unit{
+        val index = listaCookingCourse.indexOf(listaCookingCourse.find { it.name==name })
+        if (index != -1){
+            if (name.isNotEmpty()&& description.isNotEmpty()){
+                val newEntry = CookingCourse.newEntry(name, description)
+                listaCookingCourse.add(newEntry)
+                CookingCourse.message("El curso con el nombre ${newEntry.name} se registro correctamente")
+            }else{
+                CookingCourse.message("Ocurrio un error en el sistema")
+            }
         }else{
-            CookingCourse.message("Ocurrio un error en el sistema")
+            CookingCourse.message("Este curso ya existe en el sistema")
         }
     }
 
-    fun updateCourse(id:Int, name: String,description: String){
+    fun updateCourse(id:Int, name: String,description: String):Unit{
         val index = listaCookingCourse.indexOf(listaCookingCourse.find { it.id == id })
-        listaCookingCourse[index].name = name
-        listaCookingCourse[index].description = description
-        CookingCourse.message("Se actualizo correctamente")
+        if (index!= -1){
+            listaCookingCourse[index].name = name
+            listaCookingCourse[index].description = description
+            CookingCourse.message("Se actualizo correctamente")
+        }else{
+            CookingCourse.message("El registro con el id ${id} no existe")
+        }
     }
-    
-}
-
-fun main(args: Array<String>) {
 
 }
