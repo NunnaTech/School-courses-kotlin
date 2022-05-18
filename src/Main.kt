adee41f01d7f02044775a11495fc172961b30070
unknown <20193tn070@utez.edu.mx>
2022-05-18 16:28:32 -0500

    Add and get course added

--- src/Main.kt
+++ src/Main.kt
@@ -1,24 +1,33 @@
-
-
-fun main(args:Array<String>){
+fun main(args: Array<String>) {
+    createCourseProgramming("Java", "Desde 0")
     createCourseProgramming("Java", "Desde 0")
     createCourseProgramming("C", "Desde 0")
+    createCourseProgramming("C", "Desde 0")
+
+    selectCourseProgramming("fdgfdg")
 }
 
-fun createCourseProgramming(name:String, description:String){
-    val _name = name.toUpperCase()
-    var flag:Boolean = false;
-    for (e in listProgramingCourse){
-        if(e.name.equals(_name)){
+fun createCourseProgramming(name: String, description: String) {
+    var flag: Boolean = false;
+    for (e in listProgramingCourse) {
+        if (e.name.toUpperCase() == name.toUpperCase()) {
             flag = true
             break
         }
     }
-
-    if(flag){
+    if (flag) ProgramingCourse.message("ERROR: El curso ya existe")
+    else {
+        listProgramingCourse.add(ProgramingCourse.newEntry(name, description))
+        ProgramingCourse.message("ÉXITO: El curso fue añadido")
     }
+}
 
-    val programingCourse: ProgramingCourse = ProgramingCourse.newEntry(name, description)
-    println(programingCourse.toString())
+fun selectCourseProgramming(query: String) {
+    val courseSearched: ProgramingCourse? = listProgramingCourse.find{it.name == query}
+    if(courseSearched != null){
+        println("ÉXITO: Curso encontrado ${courseSearched.name} - ${courseSearched.description}")
+    }else{
+        ProgramingCourse.message("ERROR: El curso no fue encontrado")
+    }
 }
 
