1ded416bee8511654e5d494a733c854fcb96e2a5
JairDavid <20193tn071@utez.edu.mx>
2022-05-18 16:42:37 -0500

    update method added

--- src/Main.kt
+++ src/Main.kt
@@ -3,6 +3,7 @@
 fun main(args:Array<String>){
     createCourseProgramming("Java", "Desde 0")
     createCourseProgramming("C", "Desde 0")
+    updateProgrammingCourse("Java")
 }
 
 fun createCourseProgramming(name:String, description:String){
@@ -20,5 +21,34 @@ fun createCourseProgramming(name:String, description:String){
 
     val programingCourse: ProgramingCourse = ProgramingCourse.newEntry(name, description)
     println(programingCourse.toString())
+}
+
+fun updateProgrammingCourse (name:String){
+    var _name = name.toUpperCase()
+
+    var oldProgramingCourse:ProgramingCourse? = listProgramingCourse.find{ it.name.toUpperCase() == _name }
+    println(oldProgramingCourse.toString())
+    if ( oldProgramingCourse != null){
+        println("Ingresa el nuevo nombre: ")
+        var newName:String?  = readLine()
+        println("Ingresa la nueva description: ")
+        var newDescription:String? = readLine()
+
+        for (item in listProgramingCourse){
+            if(item.name.toUpperCase().equals(_name)){
+                if (newName != null && newDescription != null) {
+                    item.name = newName
+                    item.description = newDescription
+                };
+            }
+        }
+        println("Actualizado correctamente.")
+    }else{
+        println("Curso no encontrado!")
+    }
+}
+
+fun deleteProgrammingCourse(){
+
 }
 
